e52cce03c64d9b4d936f0b87db540615d8d13fc1
Evan Palmer <evanp922@gmail.com>
2025-04-10 17:39:58 -0700

    fixed copyright date

--- examples/parse_model.cpp
+++ examples/parse_model.cpp
@@ -1,4 +1,4 @@
-// Copyright 2024, Evan Palmer
+// Copyright 2025, Evan Palmer
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
